b9e2958f5c0fe948ef5144f9458031a1249adc4f
Ubuntu <michael@docker-1.roessler.link>
2025-03-31 16:12:20 +0200

    2025-03-31

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-curl -s -H "Authorization: Bearer $MEALIE_TOKEN" https://$MEALIE_URL/api/recipes | jq '.items[].slug' | tr -d '"' > recipes_list
+curl -s -H "Authorization: Bearer $MEALIE_TOKEN" https://$MEALIE_URL/api/recipes\?perPage=500 | jq '.items[].slug' | tr -d '"' > recipes_list
 while read RECIPE; do wget --quiet --directory-prefix=temp "https://$MEALIE_URL/g/home/r/${RECIPE}"; done < recipes_list;
 rsync -vaie "ssh -o LogLevel=ERROR -o StrictHostKeyChecking=no -i $RSYNC_SSH_KEY" --delete --recursive ./temp/ $RSYNC_USER@$RSYNC_HOST:mealie
 exit 0
