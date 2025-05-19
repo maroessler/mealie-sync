7900872bcd1703aaf27d64741d7cb8fd1a1806e5
Michael Roessler <maroessler@aol.com>
2025-05-19 20:21:15 +0200

    2025-05-19

--- sync.sh
+++ sync.sh
@@ -2,6 +2,6 @@
 
 curl -s -H "Authorization: Bearer $MEALIE_TOKEN" https://$MEALIE_URL/api/recipes\?perPage=500 | jq '.items[].slug' | tr -d '"' > recipes_list
 while read RECIPE; do wget --quiet --directory-prefix=temp "https://$MEALIE_URL/g/home/r/${RECIPE}"; done < recipes_list;
-sed -in "s/${MEALIE_URL}/extern-${MEALIE_URL}/g" ./temp/*
+sed -i "s/${MEALIE_URL}/extern-${MEALIE_URL}/g" ./temp/*
 rsync -vaie "ssh -o LogLevel=ERROR -o StrictHostKeyChecking=no -i $RSYNC_SSH_KEY" --delete --recursive ./temp/ $RSYNC_USER@$RSYNC_HOST:mealie
 exit 0
